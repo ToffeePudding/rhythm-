9ee7736e31db0c915deffaa1685f25617c2114c8
ToffeePudding <125533925+ToffeePudding@users.noreply.github.com>
2023-03-18 16:52:48 +0000

    some lines of code to make it more dynamic

--- main.py
+++ main.py
@@ -7,30 +7,35 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 @app.route('/')
 def index():
-    # get the list of songs in the uploads folder
-    songs = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('index.html', songs=songs)
+  # get the list of songs in the uploads folder
+  songs = os.listdir(app.config['UPLOAD_FOLDER'])
+  return render_template('index.html', songs=songs)
+
 
 @app.route('/play', methods=['POST'])
 def play():
-    song = request.form['song']
-    return render_template('play.html', song=song)
+  song = request.form['song']
+  return render_template('play.html', song=song)
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+  return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['file']
-    if file:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for('index'))
+  file = request.files['file']
+  if file:
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
 
 app.run(host='0.0.0.0', port=81)
